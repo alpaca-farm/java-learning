19cdda239e77dcdbea90b78ec46e98d1ab047910
alpaca-farm <34408081+alpaca-farm@users.noreply.github.com>
2018-02-17 17:01:58 -0800

    added a guesser class

--- src/myclass.java
+++ src/myclass.java
@@ -3,20 +3,35 @@ import java.util.Random;
 
 public class myclass {
     public static void main(String args[]) {
-    Random rand = new Random();
-    int y = rand.nextInt(10)+1;
-    int x;
-    System.out.println("give me your number");
-    Scanner in = new Scanner (System.in);
-    x = in.nextInt();
-    if (x == y){
-        System.out.println("you guess it right! random number was " + y);
+        Random rand = new Random();
+        int x;
+        int y;
+        char response='y';
 
-    }
-    else
-    {
-        System.out.println("better luck next time! random number was " + y);
+        while (true) {
+            if (response == 'y'|| response == 'Y')
+            {
+                y = rand.nextInt(10) + 1;
+                System.out.println("give me your number");
+                Scanner in = new Scanner(System.in);
+                x = in.nextInt();
+                if (x == y) {
+                    System.out.println("you guess it right! random number was " + y);
+                    System.out.println("type \"y\" to play again");
+                    response =in.next().charAt(0);
 
-    }
+
+                } else {
+                    System.out.println("better luck next time! random number was " + y);
+                    System.out.println("type \"y\" to play again");
+                    response =in.next().charAt(0);
+
+                }
+            }
+            else{
+                System.out.println("see ya next time!");
+                break;
+            }
+        }
     }
 }

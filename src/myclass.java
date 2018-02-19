715a9d3e9f6b2cb4bf7637fe46d07e2f32e295b9
alpaca-farm <34408081+alpaca-farm@users.noreply.github.com>
2018-02-18 20:03:56 -0800

    made it better

--- src/myclass.java
+++ src/myclass.java
@@ -12,21 +12,17 @@ public class myclass {
             if (response == 'y'|| response == 'Y')
             {
                 y = rand.nextInt(10) + 1;
-                System.out.println("give me your number");
+                System.out.println("guess a number between 1 and 10");
                 Scanner in = new Scanner(System.in);
-                x = in.nextInt();
-                if (x == y) {
-                    System.out.println("you guess it right! random number was " + y);
-                    System.out.println("type \"y\" to play again");
-                    response =in.next().charAt(0);
-
-
-                } else {
-                    System.out.println("better luck next time! random number was " + y);
-                    System.out.println("type \"y\" to play again");
-                    response =in.next().charAt(0);
-
+                while (!in.hasNextInt()) {
+                    System.out.println("dude, that's not a number, try again.");
+                    in.nextLine();
                 }
+                x = in.nextInt();
+                System.out.println(x == y ? "you guess it right! random number was " + y : "better luck next time. random number was " + y);
+                System.out.println("type \"y\" to play again");
+                response =in.next().charAt(0);
+
             }
             else{
                 System.out.println("see ya next time!");
